d0ced99ca967610d4b449ca89f7a1616c579900b
kiranbhalerao123 <bhaleraok65@gmail.com>
2018-08-22 21:19:48 +0530

    removed context

---
+++ src/context/context.js
@@ -0,0 +1,3 @@
+export const FETCH_WEATHER='FETCH_WEATHER';
+export const DARKSKY_API_KEY = '381e69c1a7659367d4f9977d1a611497';
+export const MAPMYINDIA_API_KEY = 'xnqlc7w5hygrzo3lbunfvm7qsgm4fhoo';
